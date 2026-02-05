180ad5da6e781d3f11ca7f7a67a70bc59270d71e
mac-pac <marcuscave0112@gmail.com>
2026-02-05 23:36:52 +1100

    Added JS functions to produce normal calculations.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let numberA = 0;
-let numberB = 0;
+let numberA = "";
+let numberB = "";
 let operator = "";
+let result
+let operatorSet = false;
 
 function add(a, b) {
     return a + b;
@@ -11,22 +13,121 @@ function subtract(a, b) {
 };
 
 function multiply(a, b) {
-    return a*b;
+    return a * b;
 };
 
-function divide(a, b){
+function divide(a, b) {
     return a / b;
 };
 
 function operate(a, operator, b) {
+
+    console.log("**************")
+    console.log(`numberA: ${numberA}`);
+    console.log(`Operator: ${operator}`);
+    console.log(`numberB: ${numberB}`);
+    console.log("**************")
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+
     switch (operator) {
         case "+":
-            return add(a, b);
+            result = add(a, b);
+            break;
         case "-":
-            return subtract(a, b);
+            result = subtract(a, b);
+            break;
         case "*":
-            return multiply(a, b);
+            result = multiply(a, b);
+            break;
         case "/":
-            return divide(a, b);
+            result = divide(a, b);
+            break;
+    };
+
+    numberA = "";
+    numberB = "";
+
+    display.textContent = result;
+
+    operatorSet = false;
+
+
+    return result;
+};
+
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        handleButtonPress(button.id, button.value)
+    })
+})
+
+function handleButtonPress(type, value) {
+    switch (type) {
+        case "number":
+            if (!operatorSet) {
+                display.textContent = "";
+                numberA += value;
+                display.textContent = numberA;
+            } else {
+                display.textContent = "";
+                numberB += value;
+                display.textContent = numberB;
+            }
+            break;
+        case "operator":
+            if (!numberA && !result) {
+                console.log("NO numA or RESULT")
+                break;
+            }
+
+            if (!numberA) {
+                console.log("TEST")
+                numberA = result;
+                operator = value;
+                console.log(`Operator: ${operator}`);
+                operatorSet = true;
+                display.textContent = "";
+
+                break;
+            }
+
+            if (!operatorSet) {
+                operator = value;
+                console.log(`Operator: ${operator}`);
+                operatorSet = true;
+                //display.textContent += value;
+                break;
+            };
+
+            operate(numberA, operator, numberB);
+            operator = value;
+            operatorSet = true;
+            numberA = result;
+
+            break;
+        case "equal":
+
+            if (!numberA) break;
+            if (!numberB) break;
+
+            operate(numberA, operator, numberB);
+            break;
+        case "clear":
+            clearScreen();
+            break;
     }
 };
+
+function clearScreen() {
+    numberA = "";
+    numberB = "";
+    operator = "";
+    result = null;
+    display.textContent = "";
+
+}
