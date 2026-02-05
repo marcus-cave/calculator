4f81de97f24637f0031098e276a2cc79fccedf2e
mac-pac <marcuscave0112@gmail.com>
2026-02-05 22:26:38 +1100

    Added skeleton calculator design

---
+++ script.js
@@ -0,0 +1,32 @@
+let numberA = 0;
+let numberB = 0;
+let operator = "";
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a*b;
+};
+
+function divide(a, b){
+    return a / b;
+};
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+};
